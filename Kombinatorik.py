c80a412898d221de2882007c9b63004c9ae5c0fd
Benjamin Mair <benjamin.mair80@gmail.com>
2025-03-25 09:09:01 +0100

    added first code

---
+++ Kombinatorik.py
@@ -0,0 +1,26 @@
+'''
+Von: Elias Frank und Benjamin Mair
+Ziel: Das Projekt soll eine Eingabe (z.B. aabc) haben und alle möglichen Kombinationen ausgeben
+        Der Input darf doppelte Zeichen enthalten
+'''
+
+def print_combis(length, chars):
+    var = chars
+    
+    for 
+    
+    print(var)
+    return
+
+length = int(input("Geben Sie die Länge der Kombination an (maximal 6 Zeichen):"))
+if length >6:
+    length = 6
+
+while True:
+    chars = input("Geben Sie eine Zeichenkette ein:")
+    if len(chars) != length:
+        print("geben Sie eine Zeichenkette mit der angegebenen Länge ein")
+    else:
+        break
+    
+print_combis(length, chars)
