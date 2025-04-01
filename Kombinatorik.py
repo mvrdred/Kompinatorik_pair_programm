fc1db79b03ffa8e3783c37e66df4b88c15a25e3f
Benjamin Mair <benjamin.mair80@gmail.com>
2025-04-01 09:26:46 +0200

    finished

--- Kombinatorik.py
+++ Kombinatorik.py
@@ -4,23 +4,59 @@ Ziel: Das Projekt soll eine Eingabe (z.B. aabc) haben und alle möglichen Kombin
         Der Input darf doppelte Zeichen enthalten
 '''
 
-def print_combis(length, chars):
-    var = chars
-    
-    for 
-    
-    print(var)
-    return
+import tkinter as tk
 
-length = int(input("Geben Sie die Länge der Kombination an (maximal 6 Zeichen):"))
-if length >6:
-    length = 6
-
-while True:
-    chars = input("Geben Sie eine Zeichenkette ein:")
-    if len(chars) != length:
-        print("geben Sie eine Zeichenkette mit der angegebenen Länge ein")
+#funktion zur berechnung der kombinationen
+def print_combis(chars):
+    combis = []
+    if len(chars) > 2:
+        for i in range(0, len(chars)):
+            var = chars
+            var = var.replace(chars[i], "")
+            combis2 = print_combis(var)
+            for combi in combis2:
+                combis.append(chars[i] + combi)
     else:
-        break
+        combis.append(chars)
+        combis.append(chars[1] + chars[0])
+    return combis
+
+#berechen knopf funtion
+def eingabe():
+    chars = ein_chars.get()
+    if len(chars) > 7:
+        erg.config(text="Die Zeichenkette darf höchstens 7 Zeichen lang sein.")
+        return
+
+    combis = print_combis(chars)
+    combi_count = len(combis)
+
+    erg.config(text=f"Anzahl der einzigartigen Kombinationen: {combi_count}")
     
-print_combis(length, chars)
+    ausgabefeld.delete(0, tk.END)
+    for combi in combis:
+        ausgabefeld.insert(tk.END, combi)
+
+#Graphische oberfläche
+root = tk.Tk()
+root.title("Kombinationen Rechner")
+
+text = tk.Label(root, text="Geben Sie eine Zeichenkette ein (max. 7 Zeichen):")
+text.grid(row=0, column=0)
+
+ein_chars = tk.Entry(root)
+ein_chars.grid(row=0, column=1)
+
+button = tk.Button(root, text="Berechnen", command=eingabe)
+button.grid(row=1, column=0, columnspan=2)
+
+erg = tk.Label(root, text="Anzahl der einzigartigen Kombinationen: ")
+erg.grid(row=2, column=0, columnspan=2)
+
+ausgabefeld = tk.Listbox(root, width=50, height=10)
+ausgabefeld.grid(row=3, column=0, columnspan=2)
+
+root.mainloop()
+
+
+
